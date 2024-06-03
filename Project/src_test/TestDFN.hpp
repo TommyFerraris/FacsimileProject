be85dc8a445dcca2e84295de8f90778d1f60e735
unknown <s297720@studenti.polito.it>
2024-06-03 11:23:26 +0200

    alcuni test su funzioni

--- Project/src_test/TestDFN.hpp
+++ Project/src_test/TestDFN.hpp
@@ -102,3 +102,91 @@ TEST(NormalePoligonoTest, PoligonoPianoXZ) {
     ASSERT_EQ(normalePoligono(poligono), normale);
 }
 
+//Test per controllo della funzione che controlla se un punto è interno o meno ad un poligono
+TEST(PuntoInternoPoligonoTest, PoligonoPianoXY){
+    vector<Vector3d> poligono = {
+        Vector3d(0, 0, 0),
+        Vector3d(1, 0, 0),
+        Vector3d(1, 1, 0),
+        Vector3d(0, 1, 0)
+    };
+    Vector3d punto(0.5, 0.5, 0);
+    ASSERT_TRUE(puntoInternoPoligono(punto, poligono));
+}
+
+TEST(PuntoInternoPoligonoTest, PoligonoPianoXZ){
+    vector<Vector3d> poligono = {
+        Vector3d(0, 0, 0),
+        Vector3d(0, 0, 1),
+        Vector3d(1, 0, 1),
+        Vector3d(1, 0, 0)
+    };
+    Vector3d punto(0.5, 0, 0.5);
+    ASSERT_TRUE(puntoInternoPoligono(punto, poligono));
+}
+
+TEST(PuntoInternoPoligonoTest, PoligonoPianoYZ){
+    vector<Vector3d> poligono = {
+        Vector3d(0, 0, 0),
+        Vector3d(0, 1, 0),
+        Vector3d(0, 1, 1),
+        Vector3d(0, 0, 1)
+    };
+    Vector3d punto(0, 0.5, 0.5);
+    ASSERT_TRUE(puntoInternoPoligono(punto, poligono));
+}
+
+//Test per controllare il punto p1 appartiane al triangolo formato da altri tre punti
+TEST(PuntoInternoTriangoloTest, TriangoloXY){
+    Vector3d p1(0.3, 0.3, 0);
+    Vector3d p2(0, 0, 0);
+    Vector3d p3(1, 0, 0);
+    Vector3d p4(0, 1, 0);
+    ASSERT_TRUE(puntointriangolo(p1, p2, p3, p4));
+}
+TEST(PuntoInternoTriangoloTest, TriangoloXZ){
+    Vector3d p1(0.3, 0, 0.3);
+    Vector3d p2(0, 0, 0);
+    Vector3d p3(0, 0, 1);
+    Vector3d p4(1, 0, 0);
+    ASSERT_TRUE(puntointriangolo(p1, p2, p3, p4));
+}
+TEST(PuntoInternoTriangoloTest, TriangoloYZ){
+    Vector3d p1(0, 0.3, 0.3);
+    Vector3d p2(0, 0, 0);
+    Vector3d p3(0, 1, 0);
+    Vector3d p4(0, 0, 1);
+    ASSERT_TRUE(puntointriangolo(p1, p2, p3, p4));
+}
+
+//Test sulla funzione che controlla se tre punti siano collineari, ovvero controlla che il punto p3 appartenga al segmento generato dai punti p1 e p2
+TEST(PuntoInSegmentoTestTrue, PianoXYZ){
+    Vector3d p1(0, 0, 0);
+    Vector3d p2(1, 1, 1);
+    Vector3d p3(0.5, 0.5, 0.5);
+    ASSERT_TRUE(puntoInSegmento(p1, p2, p3));
+}
+TEST(PuntoInSegmentoTestFalse, PianoXYZ){
+    Vector3d p1(0, 0, 0);
+    Vector3d p2(1, 1, 1);
+    Vector3d p3(0, 0, 1);
+    ASSERT_FALSE(puntoInSegmento(p1, p2, p3));
+}
+
+//Test per controllare il riordinamento delle tracce
+// TEST(RiordinaTracceTest, VettoreLunghezze){
+//     vector<double> lunghezza = {10.0, 5.65, 7.91};
+//     vector<Vector2i> tipo = {
+//         {false, lunghezza[0]},
+//         {true, lunghezza[1]},
+//         {true, lunghezza[2]}
+//     };
+
+//     // Ordine atteso: passanti ordinati per lunghezza + non passanti ordinati per lunghezza
+//     vector<Vector2i> vettoreOrdinato = {
+//         {false, lunghezza[0]}, // 10.0
+//         {true, lunghezza[2]},  // 7.91
+//         {true, lunghezza[1]}   // 5.65
+//     };
+//     ASSERT_EQ(riordinaTracce(lunghezza, tipo), vettoreOrdinato);
+// }
