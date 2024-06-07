8ac9f94a1ceba1961426ac9f889a8dd5437133fe
Andrea aerdna <s295621@studenti.polito.it>
2024-06-07 12:04:04 +0200

    Alcuni test sulle funzioni

--- Project/src/GeometryDFN.hpp
+++ Project/src/GeometryDFN.hpp
@@ -11,14 +11,14 @@ struct Struttura_DFN{
     unsigned int numFratture = 0;
     vector<unsigned int> Id_Fratture = {}; /// dimensione 1 x numFratture
     vector<unsigned int> numVertici = {}; /// dimensione 1 x numFratture
-    map <unsigned int, vector<Vector3d>> coordinateVertici = {}; /// marker Id_fratture, dimensione (3 x numVertici) x numFratture
+    map <unsigned int, vector<Vector3d>> coordinateVertici = {}; /// chiave Id_fratture, dimensione (3 x numVertici) x numFratture
 
     unsigned int numTracce = 0;
     vector<unsigned int> Id_Traccia = {}; /// dimensione 1 x numTracce
     vector<Vector2i> Id_Fratture_Intersecanti = {}; /// dimensione 2 x numTracce
-    map <unsigned int, array<Vector3d, 2>> coordinateTraccia = {}; /// marker Id_traccia, dimensione (3 x 2) x numTracce
+    map <unsigned int, array<Vector3d, 2>> coordinateTraccia = {}; /// chiave Id_traccia, dimensione (3 x 2) x numTracce
     vector<double> lunghezzaTraccia = {}; /// dimensione 1 x numTracce
-    map <unsigned int, vector<Vector2i>> tipoTraccia = {}; /// dimensione 2 x (2 x numTracce)
+    map <unsigned int, vector<Vector2i>> tipoTraccia = {}; /// chiave Id_fratture, dimensione 2 x (2 x numTracce)
     vector<unsigned int> Id_FrattureConTraccia = {}; /// dimensione 1 x dimensione TipoTraccia
 };
 
--- Project/src_test/TestDFN.hpp
+++ Project/src_test/TestDFN.hpp
@@ -8,6 +8,7 @@
 #include "Eigen/Eigen"
 #include "cmath"
 #include "algorithm"
+#include "MergeSort.hpp"
 
 using namespace std;
 using namespace DFN_Library;
@@ -160,33 +161,177 @@ TEST(PuntoInternoTriangoloTest, TriangoloYZ){
 }
 
 //Test sulla funzione che controlla se tre punti siano collineari, ovvero controlla che il punto p3 appartenga al segmento generato dai punti p1 e p2
-TEST(PuntoInSegmentoTestTrue, PianoXYZ){
+TEST(PuntoInSegmentoTest, TestTrue){
     Vector3d p1(0, 0, 0);
     Vector3d p2(1, 1, 1);
     Vector3d p3(0.5, 0.5, 0.5);
     ASSERT_TRUE(puntoInSegmento(p1, p2, p3));
 }
-TEST(PuntoInSegmentoTestFalse, PianoXYZ){
+TEST(PuntoInSegmentoTest, TestFalse){
     Vector3d p1(0, 0, 0);
     Vector3d p2(1, 1, 1);
     Vector3d p3(0, 0, 1);
     ASSERT_FALSE(puntoInSegmento(p1, p2, p3));
 }
 
-//Test per controllare il riordinamento delle tracce
-// TEST(RiordinaTracceTest, VettoreLunghezze){
-//     vector<double> lunghezza = {10.0, 5.65, 7.91};
-//     vector<Vector2i> tipo = {
-//         {false, lunghezza[0]},
-//         {true, lunghezza[1]},
-//         {true, lunghezza[2]}
-//     };
+// Test per controllare il riordinamento delle tracce
+TEST(RiordinaTracceTest, TestBase){
+    vector<double> lunghezza = {5.65, 10.0, 7.91};
+    vector<Vector2i> tipo = {
+        Vector2i{0, true},
+        Vector2i{1, true},
+        Vector2i{2, false}
+    };
 
-//     // Ordine atteso: passanti ordinati per lunghezza + non passanti ordinati per lunghezza
-//     vector<Vector2i> vettoreOrdinato = {
-//         {false, lunghezza[0]}, // 10.0
-//         {true, lunghezza[2]},  // 7.91
-//         {true, lunghezza[1]}   // 5.65
-//     };
-//     ASSERT_EQ(riordinaTracce(lunghezza, tipo), vettoreOrdinato);
-// }
+    // Ordine atteso: passanti ordinati per lunghezza + non passanti ordinati per lunghezza
+    vector<Vector2i> vettoreOrdinato = {
+        Vector2i{2, false}, // 10.0
+        Vector2i{1, true},  // 7.91
+        Vector2i{0, true}   // 5.65
+    };
+    vector<Vector2i> funzione = riordinaTracce(lunghezza, tipo);
+    ASSERT_EQ(funzione.size(), vettoreOrdinato.size());
+    for (size_t i = 0; i < funzione.size(); ++i) {
+        EXPECT_EQ(funzione[i], vettoreOrdinato[i]) << "Differ at index " << i;
+    }
+}
+
+TEST(RiordinaTracceTest, TestUgualeLunghezzaTipoDiverso){
+    vector<double> lunghezza = {10.0, 10.0};
+    vector<Vector2i> tipo = {
+        Vector2i{0, true},
+        Vector2i{1, false}
+    };
+
+    // Ordine atteso: passanti ordinati per lunghezza + non passanti ordinati per lunghezza
+    vector<Vector2i> vettoreOrdinato = {
+        Vector2i{1,false}, // 10.0 tipo passante
+        Vector2i{0, true},  // 10.0 tipo non passante
+    };
+    vector<Vector2i> funzione = riordinaTracce(lunghezza, tipo);
+    ASSERT_EQ(funzione.size(), vettoreOrdinato.size());
+    for (size_t i = 0; i < funzione.size(); ++i) {
+        EXPECT_EQ(funzione[i], vettoreOrdinato[i]) << "Differ at index " << i;
+    }
+}
+
+// Test per calcolare lunghezza tracce
+TEST(CalcolaLunghezzaTracceTest, TestBase) {
+    Struttura_DFN DFN;
+    DFN.numTracce = 2;
+    DFN.coordinateTraccia[0] = {Vector3d{0, 0, 0}, Vector3d{1, 0, 0}};
+    DFN.coordinateTraccia[1] = {Vector3d{1, 1, 1}, Vector3d{2, 0, 0}};
+    DFN.tipoTraccia[2] = {Vector2i{0, false}, Vector2i{1, true}};
+
+    calcolaLunghezzaTracce(DFN);
+
+    ASSERT_EQ(DFN.lunghezzaTraccia.size(), 2);
+    EXPECT_DOUBLE_EQ(DFN.lunghezzaTraccia[0], 1.0);
+    EXPECT_DOUBLE_EQ(DFN.lunghezzaTraccia[1], sqrt(3.0));
+}
+
+TEST(CalcolaLunghezzaTracceTest, TestLunghezzaNulla) {
+    Struttura_DFN DFN;
+    DFN.numTracce = 2;
+    DFN.coordinateTraccia[0] = {Vector3d{0, 0, 0}, Vector3d{0, 0, 0}};
+    DFN.coordinateTraccia[1] = {Vector3d{1, 1, 1}, Vector3d{4, 5, 1}};
+    DFN.tipoTraccia[2] = {Vector2i{0, false}, Vector2i{1, true}};
+
+    calcolaLunghezzaTracce(DFN);
+
+    ASSERT_EQ(DFN.lunghezzaTraccia.size(), 2);
+    EXPECT_DOUBLE_EQ(DFN.lunghezzaTraccia[0], 0.0);
+    EXPECT_DOUBLE_EQ(DFN.lunghezzaTraccia[1], 5.0);
+}
+
+// Funzione che testi se calcolo in maniera correta la tipologia delle tracce e la salvo
+TEST(CalcolaTipologiaTracceTest, FunzioneBase) {
+    Struttura_DFN DFN;
+    DFN.numFratture = 2;
+    DFN.numTracce = 3;
+    DFN.numVertici = {3, 4};
+
+    // Coordinate delle tracce
+    DFN.coordinateTraccia[0] ={Vector3d(1, 1, 0), Vector3d(2, 2, 0)};
+    DFN.coordinateTraccia[1] ={Vector3d(3, 3, 0), Vector3d(4, 4, 0)};
+    DFN.coordinateTraccia[2] ={Vector3d(5, 5, 0), Vector3d(6, 6, 0)};
+
+    // Coordinate dei vertici delle fratture
+    DFN.coordinateVertici[0] = {Vector3d(0, 0, 0), Vector3d(2, 0, 0), Vector3d(1, 1, 0)};
+    DFN.coordinateVertici[1] = {Vector3d(3, 3, 0), Vector3d(5, 3, 0), Vector3d(5, 5, 0), Vector3d(3, 5, 0)};
+
+    // Id delle fratture intersecanti
+    DFN.Id_Fratture_Intersecanti = {
+        {0, 1},
+        {0, 1},
+        {1, 0}
+    };
+
+    // Chiamata della funzione da testare
+    calcolaTipologiaTracce(DFN);
+
+    // Fratture con traccia attese
+    vector<int> Id_FrattureConTraccia_Expected = {0, 1};
+
+    // Verifica
+    ASSERT_EQ(DFN.Id_FrattureConTraccia.size(), Id_FrattureConTraccia_Expected.size());
+    for (size_t i = 0; i < DFN.Id_FrattureConTraccia.size(); ++i) {
+        EXPECT_EQ(DFN.Id_FrattureConTraccia[i], Id_FrattureConTraccia_Expected[i]);
+    }
+
+    // Verifica delle tipologie delle tracce
+    map<unsigned int, vector<Vector2i>> tipoTraccia_Expected;
+    tipoTraccia_Expected[0] = {{0, true}, {1, true}, {2, true}};
+    tipoTraccia_Expected[1] = {{0, true}, {1, true}, {2, true}};
+
+    ASSERT_EQ(DFN.tipoTraccia.size(), tipoTraccia_Expected.size());
+    for (size_t i = 0; i < DFN.tipoTraccia.size(); ++i) {
+        ASSERT_EQ(DFN.tipoTraccia[i].size(), tipoTraccia_Expected[i].size());
+        for (size_t j = 0; j < DFN.tipoTraccia[i].size(); ++j) {
+            EXPECT_EQ(DFN.tipoTraccia[i][j], tipoTraccia_Expected[i][j]) << "Differ at index " << i << ", " << j;
+        }
+    }
+}
+
+TEST(CalcolaTipologiaTracceTest, FunzioneConPassante) {
+    Struttura_DFN DFN;
+    DFN.numFratture = 2;
+    DFN.numTracce = 1;
+    DFN.numVertici = {4, 4};
+
+    // Coordinate delle tracce
+    DFN.coordinateTraccia[0] ={Vector3d(0.8, 0, 0), Vector3d(0.8, 1, 0)};
+
+    // Coordinate dei vertici delle fratture
+    DFN.coordinateVertici[0] = {Vector3d(0, 0, 0), Vector3d(1, 0, 0), Vector3d(1, 1, 0), Vector3d(0, 1, 0)};
+    DFN.coordinateVertici[1] = {Vector3d(0.8, 0, -1), Vector3d(0.8, 0, 0.3), Vector3d(0.8, 1, 0.3), Vector3d(0.8, 1, -1)};
+
+    // Id delle fratture intersecanti
+    DFN.Id_Fratture_Intersecanti = {{0, 1}};
+
+    // Chiamata della funzione da testare
+    calcolaTipologiaTracce(DFN);
+
+    // Fratture con traccia attese
+    vector<int> Id_FrattureConTraccia_Expected = {0, 1};
+
+    // Verifica
+    ASSERT_EQ(DFN.Id_FrattureConTraccia.size(), Id_FrattureConTraccia_Expected.size());
+    for (size_t i = 0; i < DFN.Id_FrattureConTraccia.size(); ++i) {
+        EXPECT_EQ(DFN.Id_FrattureConTraccia[i], Id_FrattureConTraccia_Expected[i]);
+    }
+
+    // Verifica delle tipologie delle tracce
+    map<unsigned int, vector<Vector2i>> tipoTraccia_Expected;
+    tipoTraccia_Expected[0] = {{0, false}};
+    tipoTraccia_Expected[1] = {{0, false}};
+
+    ASSERT_EQ(DFN.tipoTraccia.size(), tipoTraccia_Expected.size());
+    for (size_t i = 0; i < DFN.tipoTraccia.size(); ++i) {
+        ASSERT_EQ(DFN.tipoTraccia[i].size(), tipoTraccia_Expected[i].size());
+        for (size_t j = 0; j < DFN.tipoTraccia[i].size(); ++j) {
+            EXPECT_EQ(DFN.tipoTraccia[i][j], tipoTraccia_Expected[i][j]) << "Differ at index " << i << ", " << j;
+        }
+    }
+}
+
